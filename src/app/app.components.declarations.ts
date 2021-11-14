33af1820cf5093dbc65a0713c584d3ff3b878f00
Axel Faya <73588143+axelfaya@users.noreply.github.com>
2021-11-14 01:11:58 +0100

    fix : move layout folder into shared module

--- src/app/app.components.declarations.ts
+++ src/app/app.components.declarations.ts
@@ -1,7 +1,7 @@
 import { AppComponent } from './app.component';
-import { layout as fromLayout } from './core/layout/index';
+import { layout as fromLayout } from './shared/layout/index';
 
 export const components: Array<any> = [AppComponent, ...fromLayout];
 
 export * from './app.component';
-export * from './core/layout/index';
+export * from './shared/layout/index';
